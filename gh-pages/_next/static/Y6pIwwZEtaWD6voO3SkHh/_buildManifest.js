d758d2482d517e6c636baefff1ba438fa7888b4f
Ioodu <chinadbo@foxmail.com>
2026-01-31 07:50:33 +0000

    Fix asset paths for GitHub Pages deployment

--- gh-pages/_next/static/Y6pIwwZEtaWD6voO3SkHh/_buildManifest.js
+++ gh-pages/_next/static/Y6pIwwZEtaWD6voO3SkHh/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/AI-StartUP//_next/:path+",
+        "destination": "/AI-StartUP/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
